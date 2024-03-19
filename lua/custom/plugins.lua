c3f8e4e0a34f90c0ca94c48b20ddfce9b81fa4ff
DAWSTAD <dawid.stade@mercedes-benz.com>
2024-03-19 14:17:14 +0100

    Modify telescope to avoid crashes when searching in current buffer

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,5 +1,26 @@
 local plugins = {
   {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    cmd = "Telescope",
+    init = function()
+      require("core.utils").load_mappings "telescope"
+    end,
+    opts = function()
+      return require "custom.configs.telescope"
+    end,
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. "telescope")
+      local telescope = require "telescope"
+      telescope.setup(opts)
+
+      -- load extensions
+      for _, ext in ipairs(opts.extensions_list) do
+        telescope.load_extension(ext)
+      end
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     config = function()
       require "plugins.configs.lspconfig"
